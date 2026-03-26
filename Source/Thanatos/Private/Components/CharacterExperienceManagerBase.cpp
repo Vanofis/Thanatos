f07ab6f340684f4b4475d0947c26aa9305fa62b1
Vanofis <synergyivanb@gmail.com>
2026-03-26 15:34:12 +0100

    existing code refactor

---
+++ Source/Thanatos/Private/Components/CharacterExperienceManagerBase.cpp
@@ -0,0 +1,62 @@
+﻿#include "CharacterExperienceManagerBase.h"
+#include "Data/CharacterExperience.h"
+
+UCharacterExperienceManagerBase::UCharacterExperienceManagerBase()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UCharacterExperienceManagerBase::BeginPlay()
+{
+	if (ActiveExperience)
+	{
+		ApplyExperienceActions();
+	}
+	
+	Super::BeginPlay();
+}
+
+void UCharacterExperienceManagerBase::SetCharacterExperience(UCharacterExperience* NewConfiguration)
+{
+	if (ActiveExperience)
+	{
+		UndoExperienceActions();
+	}
+	
+	ActiveExperience = NewConfiguration;
+	if (ActiveExperience)
+	{
+		ApplyExperienceActions();
+	}
+}
+
+AController* UCharacterExperienceManagerBase::GetController()
+{
+	if (Controller)
+	{
+		return Controller;
+	}
+	
+	Controller = Cast<AController>(GetOwner());
+	return Controller;
+}
+
+void UCharacterExperienceManagerBase::ApplyExperienceActions()
+{
+	for (const auto& Action : ActiveExperience->Actions)
+	{
+		ActiveActions.Add(DuplicateObject<UCharacterExperienceAction>(Action, this));
+		
+		const auto Last = ActiveActions.Last();
+		Last->Apply(GetController()->GetPawn());
+	}
+}
+
+void UCharacterExperienceManagerBase::UndoExperienceActions()
+{
+	while (!ActiveActions.IsEmpty())
+	{
+		ActiveActions.Pop()->Undo(GetController()->GetPawn());
+	}
+}
+
--- Source/Thanatos/Private/Components/CharacterExperienceManagerBase.h
+++ Source/Thanatos/Private/Components/CharacterExperienceManagerBase.h
@@ -2,19 +2,19 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "CharacterConfigurationManager.generated.h"
+#include "CharacterExperienceManagerBase.generated.h"
 
 class UCharacterExperience;
 class UCharacterExperienceAction;
 
-UCLASS(Blueprintable, Abstract, meta=(BlueprintSpawnableComponent))
-class THANATOS_API UCharacterConfigurationManager : public UActorComponent
+UCLASS(Blueprintable, Abstract)
+class THANATOS_API UCharacterExperienceManagerBase : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
-	TObjectPtr<UCharacterExperience> ActiveConfiguration;
+	TObjectPtr<UCharacterExperience> ActiveExperience;
 	
 private:
 	UPROPERTY()
@@ -24,17 +24,17 @@ private:
 	TObjectPtr<AController> Controller;
 	
 public:
-	UCharacterConfigurationManager();
+	UCharacterExperienceManagerBase();
 	
 	virtual void BeginPlay() override;
 	
 protected:
 	UFUNCTION(BlueprintCallable)
-	void SetCharacterConfiguration(UCharacterExperience* NewConfiguration);
+	void SetCharacterExperience(UCharacterExperience* NewConfiguration);
 	
 	UFUNCTION(BlueprintCallable, BlueprintPure)
 	AController* GetController();
 	
-	void ApplyConfigurationActions();
-	void UndoConfigurationActions();
+	void ApplyExperienceActions();
+	void UndoExperienceActions();
 };
