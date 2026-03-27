a750f0d4d59ff015b163c8ad06f15b740fe99eaf
Vanofis <synergyivanb@gmail.com>
2026-03-27 01:31:12 +0100

    refactored character action system (it's better now)

---
+++ Source/Thanatos/Private/CharacterActions/CharacterActionBase.cpp
@@ -0,0 +1,7 @@
+﻿#include "CharacterActionBase.h"
+
+void UCharacterActionBase::Apply_Implementation(APawn* Pawn)
+{}
+
+void UCharacterActionBase::Undo_Implementation(APawn* Pawn)
+{}
---
+++ Source/Thanatos/Private/CharacterActions/CharacterActionBase.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "CharacterActionBase.generated.h"
+
+UCLASS(Blueprintable, Abstract, DefaultToInstanced, EditInlineNew)
+class THANATOS_API UCharacterActionBase : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void Apply(APawn* Pawn);
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void Undo(APawn* Pawn);
+};
